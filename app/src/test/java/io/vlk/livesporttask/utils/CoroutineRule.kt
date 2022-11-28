5b2f48f079300c6fcb5c4a10a588a61af31b147d
Vladislav Klima <vladislav.klima@applifting.io>
2022-11-28 15:12:19 +0100

    removed unused part

--- app/src/test/java/io/vlk/livesporttask/utils/CoroutineRule.kt
+++ app/src/test/java/io/vlk/livesporttask/utils/CoroutineRule.kt
@@ -3,7 +3,6 @@ package io.vlk.livesporttask.utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -11,21 +10,6 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 //Based on https://github.com/Kotlin/kotlinx.coroutines/blob/master/kotlinx-coroutines-test/MIGRATION.md
-//@ExperimentalCoroutinesApi
-//class CoroutineRule(private val dispatcher: TestDispatcher = StandardTestDispatcher()) :
-//    TestWatcher() {
-//
-//    override fun starting(description: Description?) {
-//        super.starting(description)
-//        Dispatchers.setMain(dispatcher)
-//    }
-//
-//    override fun finished(description: Description?) {
-//        super.finished(description)
-//        Dispatchers.resetMain()
-//    }
-//}
-
 @OptIn(ExperimentalCoroutinesApi::class)
 class CoroutineRule(startPaused: Boolean = false) : TestWatcher() {
 
